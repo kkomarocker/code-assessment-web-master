1862f8251343a9da2cfbc0a10e55115d66609cea
Jay.Lee <kkomarocker@gmail.com>
2019-03-14 22:55:16 -0400

    feat: remove button added for each item

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,39 +1,43 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Product from './Product'
+import React from "react";
+import PropTypes from "prop-types";
+import Product from "./Product";
 
-const Cart  = ({ products, total, onCheckoutClicked }) => {
-  const hasProducts = products.length > 0
+const Cart = ({ products, total, onCheckoutClicked, onRemoveClicked }) => {
+  const hasProducts = products.length > 0;
   const nodes = hasProducts ? (
-    products.map(product =>
+    products.map(product => (
       <Product
         title={product.title}
         price={product.price}
         quantity={product.quantity}
         key={product.id}
+        hasProducts={hasProducts}
+        onRemoveClicked={onRemoveClicked}
       />
-    )
+    ))
   ) : (
     <em>Please add some products to cart.</em>
-  )
+  );
 
   return (
     <div>
       <h3>Your Cart</h3>
       <div>{nodes}</div>
       <p>Total: &#36;{total}</p>
-      <button onClick={onCheckoutClicked}
-        disabled={hasProducts ? '' : 'disabled'}>
+      <button
+        onClick={onCheckoutClicked}
+        disabled={hasProducts ? "" : "disabled"}
+      >
         Checkout
       </button>
     </div>
-  )
-}
+  );
+};
 
 Cart.propTypes = {
   products: PropTypes.array,
   total: PropTypes.string,
   onCheckoutClicked: PropTypes.func
-}
+};
 
-export default Cart
+export default Cart;
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,16 +1,20 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-const Product = ({ price, inventory, title }) => (
+const Product = ({ price, inventory, title, hasProducts, onRemoveClicked }) => (
   <div>
-    {title} - &#36;{price}{inventory ? ` x ${inventory}` : null}
+    {title} - &#36;{price}
+    {inventory ? ` x ${inventory}` : null}
+    {hasProducts ? (
+      <button onClick={onRemoveClicked}>Remove Item</button>
+    ) : null}
   </div>
-)
+);
 
 Product.propTypes = {
   price: PropTypes.number,
   inventory: PropTypes.number,
   title: PropTypes.string
-}
+};
 
-export default Product
+export default Product;
