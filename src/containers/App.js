4eeb505865716d75dec150d3d3da2ea03e7ce4c7
Jay.Lee <kkomarocker@gmail.com>
2019-03-16 22:06:27 -0400

    feat: adding styles

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import ProductsContainer from './ProductsContainer'
-import CartContainer from './CartContainer'
+import React from "react";
+import ProductsContainer from "./ProductsContainer";
+import CartContainer from "./CartContainer";
+import "../index.scss";
 
 const App = () => (
   <div>
-    <h2>Shopping Cart Example</h2>
-    <hr/>
+    <div className="main-title">Work & Co Store</div>
+    <hr />
     <ProductsContainer />
-    <hr/>
+    <hr />
     <CartContainer />
   </div>
-)
+);
 
-export default App
+export default App;
